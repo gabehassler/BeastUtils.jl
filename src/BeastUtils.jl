78194554cf96049e9097de4999bd66410fcfbbdd
Gabe Hassler <gabriel.w.hassler@gmail.com>
2020-04-16 11:07:28 -0700

    Files generated by PkgTemplates

---
+++ src/BeastUtils.jl
@@ -0,0 +1,5 @@
+module BeastUtils
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using BeastUtils
+using Test
+
+@testset "BeastUtils.jl" begin
+    # Write your own tests here.
+end
