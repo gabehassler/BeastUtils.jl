9341e33f431f32691eaf73134f26d30c5b202f47
Gabe Hassler <gabriel.w.hassler@gmail.com>
2020-05-04 17:59:57 -0700

    fixing test script

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,11 @@
 using Test, SafeTestsets
 
+import Pkg
+Pkg.add("Random")
+
+using RCall
+r"install.packages(\"ape\")"
+
+
 @time @safetestset "Data simulation test" begin include("diffusionSimulation_test.jl") end
 @time @safetestset "RTrees Test" begin include("RTrees_test.jl") end
