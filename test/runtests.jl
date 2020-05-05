2e5e81b2198c67ecb3735c07410cf14282ac4e9b
Gabe Hassler <gabriel.w.hassler@gmail.com>
2020-05-05 11:00:18 -0700

    installing littler in travis yml (for RScript)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,5 @@
 using Test, SafeTestsets
 
-using RCall
-r"install.packages(\"ape\")"
-
 
 @time @safetestset "Data simulation test" begin include("diffusionSimulation_test.jl") end
 @time @safetestset "RTrees Test" begin include("RTrees_test.jl") end
