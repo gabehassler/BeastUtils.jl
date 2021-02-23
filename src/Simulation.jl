40e5044c301f72ab1eeda384e98db366114880b5
Gabe Hassler <gabriel.w.hassler@gmail.com>
2021-02-22 16:36:31 -0800

    exporting random factor simulation

--- src/Simulation.jl
+++ src/Simulation.jl
@@ -5,7 +5,8 @@ export TreeDiffusionModel,
        LatentFactorModel,
        TraitSimulationModel,
        simulate,
-       get_newick
+       get_newick,
+       randomFactorSimulationModel
 
 using PhyloNetworks, LinearAlgebra, LinearAlgebra.BLAS, DataFrames, Distributions
 using BeastUtils.MatrixUtils, BeastUtils.TreeUtils
