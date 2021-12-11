df5b57a68c25b532591faab36d3b8a4dcaf5c861
Gabe Hassler <gabriel.w.hassler@gmail.com>
2021-12-10 18:20:02 -0800

    utilities for working with nexus files

--- src/Nexus.jl
+++ src/Nexus.jl
@@ -38,4 +38,91 @@ function parse_taxon_block(path::String)
     return taxa
 end
 
+function parse_taxa_dict(path::String)
+    taxa = Dict{String, String}()
+
+    past_translate_line = false
+
+    for line in eachline(path)
+        s_line = strip(line)
+        if past_translate_line && s_line == ";"
+            break
+        elseif past_translate_line && !isempty(s_line)
+            vals = split(s_line)
+            taxon_number = vals[1]
+
+            taxon_name = join(vals[2:end], ' ')
+            if endswith(taxon_name, ',')
+                taxon_name = taxon_name[1:(end - 1)]
+            end
+            taxa[taxon_name] = taxon_number
+        elseif s_line == "Translate"
+            past_translate_line = true
+        end
+    end
+    return taxa
+end
+
+
+function findsecond(c::Char, s::AbstractString)
+    first_found = false
+    for i = 1:length(s)
+        if s[i] == c
+            if first_found
+                return i
+            else
+                first_found = true
+            end
+        end
+    end
+
+    return nothing
+end
+
+import Base.strip
+function strip(::Nothing)
+    # do nothing
+end
+
+struct TreeIterator
+    lineIterator::Base.EachLine{IOStream}
+end
+
+TreeIterator(path::AbstractString) = TreeIterator(eachline(path))
+
+import Base.iterate
+
+function iterate(iterator::TreeIterator)
+    lineIterator = iterator.lineIterator
+    next_line = iterate(lineIterator)
+
+    while !isnothing(next_line) && !startswith(next_line[1], "tree STATE_")
+        next_line = iterate(lineIterator)
+    end
+
+    if isnothing(next_line)
+        return nothing
+    end
+
+    next_line = next_line[1]
+
+    @assert endswith(next_line, ';')
+    state_start = findfirst('_', next_line) + 1
+    state_end = findsecond(' ', next_line) - 1
+    state = parse(Int, @view next_line[state_start:state_end])
+    tree_start = findfirst('(', next_line)
+    tree = next_line[tree_start:end] # ends with ; by assertion
+    return ((tree, state), nothing)
+end
+
+function iterate(iterator::TreeIterator, ::Int)
+    iterate(iterator)
+end
+
+function iterate(iterator::TreeIterator, ::Nothing)
+    iterate(iterator)
+end
+
+
+
 end
