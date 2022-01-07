18d70e7f0de1929a56da1b6ef7909da8c80e3228
Gabe Hassler <gabriel.w.hassler@gmail.com>
2022-01-07 14:06:54 -0800

    RunBeast can write to file

--- src/RunBeast.jl
+++ src/RunBeast.jl
@@ -44,11 +44,21 @@ function check_beast(;beast_jar::String = find_beast())
     println("\n\nJava and BEAST checks succeeded.")
 end
 
+function default_output(xml_path::String)
+
+    if endswith(xml_path, ".xml")
+        return xml_path[1:(end - 4)] * ".out"
+    end
+    return xml_path * ".out"
+end
+
 function run_beast(xml_path::String;
                     seed::Int = -1, # don't set seed by default
                     overwrite::Bool = false, # don't overwrite log files by default
                     directory::String = pwd(), #don't change working directory by default
-                    beast_jar::String = find_beast()
+                    beast_jar::String = find_beast(),
+                    capture_output::Bool = false,
+                    output_path::String = ""
                     )
 
     old_directory = pwd()
@@ -69,9 +79,33 @@ function run_beast(xml_path::String;
 
     push!(cmds, xml_path)
 
-    out = run(Cmd(cmds))
-    if out.exitcode != 0
-        error("BEAST run threw an error. See console for error message.")
+    if !isempty(output_path)
+        capture_output = true
+    end
+
+    try
+        if capture_output
+            if isempty(output_path)
+                output_path = default_output(xml_path)
+            end
+
+            open(output_path, "w") do outio
+                    out = run(pipeline(Cmd(cmds), stdout=outio, stderr=outio))
+
+            end
+
+        else
+            out = run(pipeline(Cmd(cmds)))
+        end
+        if out.exitcode != 0
+            error("BEAST run threw an error. See console for error message.")
+        end
+    catch
+        cd(old_directory)
+        if isfile(output_path)
+            println(read(output_path, String))
+        end
+        error("An error occured while running BEAST, terminating. See BEAST output above for error details.")
     end
 
     cd(old_directory)
